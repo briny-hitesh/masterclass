bf629f02ef639ead45fe51f23d691f46871dc247
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:50:24 +0530

    Feat: wishlist test cases added

--- src/app/core/components/wishlist-icon-button/wishlist-icon-button.component.spec.ts
+++ src/app/core/components/wishlist-icon-button/wishlist-icon-button.component.spec.ts
@@ -2,23 +2,33 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { WishlistIconButtonComponent } from './wishlist-icon-button.component';
+import { WishlistService } from '@core/services/wishlist.service';
 
 describe('WishlistIconButtonComponent', () => {
   let component: WishlistIconButtonComponent;
   let fixture: ComponentFixture<WishlistIconButtonComponent>;
+  let wishlistService: WishlistService;
 
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WishlistIconButtonComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
     fixture = TestBed.createComponent(WishlistIconButtonComponent);
     component = fixture.componentInstance;
+    wishlistService = TestBed.inject(WishlistService);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be added wishlist', () => {
+    component.id = 1;
+    component.addToWishlist()
+    expect(component.wishlistIds()[0]).toBe(1);
+  });
+
+  it('should be removed from wishlist', () => {
+    component.id = 1;
+    component.removeFromWishlist()
+    expect(component.wishlistIds().length).toBe(0);
   });
 });
