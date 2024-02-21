441a7d17f09be9a474af15387b6ce95863ca6aeb
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:52:53 +0530

    Feat: cart icon component test cases integration

--- src/app/core/components/cart-icon-button/cart-icon-button.component.spec.ts
+++ src/app/core/components/cart-icon-button/cart-icon-button.component.spec.ts
@@ -1,24 +1,30 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CartService } from '@core/services/cart.service';
 import { CartIconButtonComponent } from './cart-icon-button.component';
 
 describe('CartIconButtonComponent', () => {
   let component: CartIconButtonComponent;
   let fixture: ComponentFixture<CartIconButtonComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CartIconButtonComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
+  let cartService: CartService;
+  beforeEach(() => {
+    
     fixture = TestBed.createComponent(CartIconButtonComponent);
+    cartService = TestBed.inject(CartService);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should has checkout url', () => {
+    expect(component.checkoutRoute).toBeDefined();
+  });
+
+  it('should update id in cart service', () => {
+    component.addToCart(1);
+    expect(component.cartItemsIds().includes(1)).toBeTrue();
   });
 });
