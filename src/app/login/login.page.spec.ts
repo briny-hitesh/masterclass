21a34285aa3cf4012edd833e7d78e0989a2ca1a8
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 13:25:52 +0530

    Feat: implemented login screen and basic sign in form

---
+++ src/app/login/login.page.spec.ts
@@ -0,0 +1,67 @@
+import { ComponentFixture, TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { ToastController } from "@ionic/angular";
+import { LoginPage } from './login.page';
+
+import { TOAST_CONFIG } from '@core/constants/toast.constant';
+import { Router } from '@angular/router';
+import { ROUTES } from '@core/constants/routes.constant';
+
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+  let toast: ToastController;
+  let router: Router;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(LoginPage);
+    component = fixture.componentInstance;
+    toast = TestBed.inject(ToastController);
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should throw error when username is not entered', async () => {
+    const mockToast = spyOn(toast, 'create').and.callThrough();
+    component.password = 'dummy123';
+    component.signIn();
+    expect(mockToast).toHaveBeenCalled();
+    expect(mockToast).toHaveBeenCalledWith({
+      message: 'Look like you have entered invalid username or password.',
+      ...TOAST_CONFIG
+    });
+  });
+
+  it('should throw error when password is not entered', async () => {
+    const mockToast = spyOn(toast, 'create').and.callThrough();
+    component.username = '';
+    component.signIn();
+    expect(mockToast).toHaveBeenCalled();
+    expect(mockToast).toHaveBeenCalledWith({
+      message: 'Look like you have entered invalid username or password.',
+      ...TOAST_CONFIG
+    });
+  });
+  
+  it('should redirect to home on login success', async () => {
+    const mockRouter = spyOn(router, 'navigateByUrl');
+    component.username = 'qvg';
+    component.password = 'dummy123';
+    component.signIn();
+    expect(mockRouter).toHaveBeenCalled();
+    expect(mockRouter).toHaveBeenCalledWith(`/${ROUTES.HOME}`);
+  });
+
+  it(`should toaster disappear after ${TOAST_CONFIG.duration}`, fakeAsync(() => {
+    component.signIn();
+    const Ref = toast.getTop();
+    expect(Ref).toBeTruthy();
+    tick(TOAST_CONFIG.duration - 1);
+    expect(Ref).toBeTruthy();
+    tick(3);
+    expect(Ref).toBeFalsy();
+  }));
+});
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ToastController } from "@ionic/angular";
+import { ROUTES } from '@core/constants/routes.constant';
+import { TOAST_CONFIG } from '@core/constants/toast.constant';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage {
+  #router: Router = inject(Router);
+  #toast: ToastController = inject(ToastController);
+
+  username: string = '';
+  password: string = '';
+
+  /**
+   * User sign in logic function for validation details
+   * TODO: Validate user using backend api.
+   */
+  signIn(): void {
+    if (this.username && this.password && this.password === 'dummy123') {
+      this.#router.navigateByUrl(`/${ROUTES.HOME}`);
+    } else {
+      this.#toast.create({
+        message: 'Look like you have entered invalid username or password.',
+        ...TOAST_CONFIG,
+      }).then(modal => modal.present());
+    }
+  }
+
+}
