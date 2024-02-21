7b89cbf19097069a8e770884dc0b0dfb6da4bf65
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:53:12 +0530

    Feat: notification page test cases integration

--- src/app/pages/notification/notification.page.spec.ts
+++ src/app/pages/notification/notification.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { NotificationPage } from './notification.page';
 
 describe('NotificationPage', () => {
