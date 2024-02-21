69dab415487cfcd422fa8fca40d45bb5f9c9bfbf
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:53:59 +0530

    Feat: Profile page test cases integration

--- src/app/pages/profile/profile.page.spec.ts
+++ src/app/pages/profile/profile.page.spec.ts
@@ -1,15 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ProfilePage } from './profile.page';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ProfilePage,
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProfilePage);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
