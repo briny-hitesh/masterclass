9bb3feb86c09251c99b6068c4950b2e941699d5a
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:54:20 +0530

    Feat: wishlist test case integration

--- src/app/pages/wishlist/wishlist.page.spec.ts
+++ src/app/pages/wishlist/wishlist.page.spec.ts
@@ -1,15 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WishlistPage } from './wishlist.page';
+import { DataService } from '@core/services/data.service';
+import { WishlistService } from '@core/services/wishlist.service';
 
 describe('WishlistPage', () => {
   let component: WishlistPage;
   let fixture: ComponentFixture<WishlistPage>;
+  let dataService: DataService;
+  let wishlistService: WishlistService;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        WishlistPage,
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(WishlistPage);
+    dataService = TestBed.inject(DataService);
+    wishlistService = TestBed.inject(WishlistService);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
