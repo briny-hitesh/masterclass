0f47947ccdc5397d7227872ca7fc824e5ce4a7c6
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 16:11:41 +0530

    Feat: remove static course data entry from home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule, CurrencyPipe, SlicePipe } from '@angular/common';
-import { Course } from '@core/model/course.model';
+import { Component, inject } from '@angular/core';
 
-import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
-import { ACTIONS } from '@core/constants/actions.constant';
-import { DataService } from '@core/services/data.service';
 import { CartIconButtonComponent } from '@core/components/cart-icon-button/cart-icon-button.component';
 import { WishlistIconButtonComponent } from '@core/components/wishlist-icon-button/wishlist-icon-button.component';
+import { ACTIONS } from '@core/constants/actions.constant';
+import { DataService } from '@core/services/data.service';
+import { IonicModule } from '@ionic/angular';
 
 const imports = [
   IonicModule, CommonModule, CurrencyPipe, SlicePipe, RouterLink, CartIconButtonComponent, WishlistIconButtonComponent,
@@ -20,24 +19,8 @@ const imports = [
   standalone: true,
   imports: [ ...imports ],
 })
-export class HomePage implements OnInit {
+export class HomePage {
   #dataService: DataService = inject(DataService);
   courses = this.#dataService.courses;
   actions = ACTIONS;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.#dataService.addCourses(
-      [new Course({
-        "id": 1,
-        "courseName": "Advanced Machine Learning",
-        "author": "Alex Johnson",
-        "actualPrice": 1199,
-        "currency": "INR",
-        "discountPercentage": 15,
-        "tags": ["Machine Learning", "Python"]
-      })]
-    )
-  }
 }
