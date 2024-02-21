c3f6e936a08aa06cda61fa679317d9ebd4c15a9c
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 13:25:11 +0530

    Feat: Added constants for routes and toast configuration

---
+++ src/app/core/constants/toast.constant.ts
@@ -0,0 +1,9 @@
+export const TOAST_CONFIG: {
+    keyboardClose: boolean;
+    duration: number;
+    position: "top" | "bottom" | "middle" | undefined;
+} = {
+    keyboardClose: true,
+    duration: 1000,
+    position: "top",
+}
