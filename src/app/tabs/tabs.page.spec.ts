972cf624fbefc19497294076459deb9fa9b3063b
Hitesh Patoliya <hiteshpatoliya1992@gmail.com>
2024-02-21 18:52:26 +0530

    Feat: tabs page test cases integration

--- src/app/tabs/tabs.page.spec.ts
+++ src/app/tabs/tabs.page.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabsPage } from './tabs.page';
+import { CoursesService } from '@core/services/courses.service';
+import { provideHttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TabsPage', () => {
   let component: TabsPage;
   let fixture: ComponentFixture<TabsPage>;
+  let coursesService: CoursesService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+      imports: [RouterTestingModule, RouterLink]
+    })
     fixture = TestBed.createComponent(TabsPage);
     component = fixture.componentInstance;
+    coursesService = TestBed.inject(CoursesService);
     fixture.detectChanges();
   });
 
